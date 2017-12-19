cc8919460512ebddf70baddc87bce5d41a03df0b
wupeng4321 <wupeng@mappdeMac-mini.local>
2017-12-19 16:34:25 +0800

    start jdapp

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'jdapp' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for jdapp
+
+  target 'jdappTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'jdappUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+    pod 'AFNetworking'
+end
---
+++ jdapp/Helper/BaseViewController/AllocDellocViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AllocDellocViewController.swift
+//  jdapp
+//
+//  Created by wupeng on 2017/12/19.
+//  Copyright © 2017年 wupeng. All rights reserved.
+//
+
+import UIKit
+
+class AllocDellocViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
