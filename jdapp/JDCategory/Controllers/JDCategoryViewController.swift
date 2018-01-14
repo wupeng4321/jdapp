7c8490f2c5ffce53c3b9d06949f8b667d31008c6
wupeng4321 <wupeng_4321@163.com>
2018-01-15 00:23:09 +0800

    add category left title

--- jdapp/JDCategory/Controllers/JDCategoryViewController.swift
+++ jdapp/JDCategory/Controllers/JDCategoryViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class JDCategoryViewController: BaseViewController, UITableViewDelegate, UITableViewDataSource, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    let category = ["推荐分类", "京东超市", "国际名牌", "奢侈品", "全球购", "男装", "女装", "男鞋", "女鞋", "内衣配饰", "箱包手袋", "美妆个护", "钟表珠宝", "手机数码", "电脑办公", "家用电器", "食品生鲜", "酒水饮料", "母婴童装", "玩具乐器", "医药保健", "计生情趣", "运动户外", "汽车用品", "家居厨具", "家具家装", "礼品鲜花", "宠物生活", "生活旅行", "图书音像", "邮币", "农资绿植", "特产馆", "京东金融", "拍卖", "房产", "二手商品"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = ColorFromRGB(0xf3f5f7)
@@ -24,10 +26,10 @@ class JDCategoryViewController: BaseViewController, UITableViewDelegate, UITable
             make.top.equalTo(headerView.snp.bottom)
             make.bottom.equalTo(self.view.snp.bottom).offset(-kTabBarHeight)
             make.left.equalTo(self.view)
-            make.width.equalTo(Theme.paddingWithSize(160))
+            make.width.equalTo(Theme.paddingWithSize(180))
         }
         collectionView.snp.makeConstraints { (make) -> Void in
-            make.left.equalTo(tableView.snp.right)
+            make.left.equalTo(tableView.snp.right).offset(Theme.paddingWithSize(20))
             make.top.bottom.equalTo(tableView)
             make.right.equalTo(self.view)
         }
@@ -69,12 +71,15 @@ class JDCategoryViewController: BaseViewController, UITableViewDelegate, UITable
     //MARK: - tableview delegate & datasource
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellId")
-        cell?.textLabel?.text = "\(indexPath.row)"
+        cell?.textLabel?.font = Theme.fontWithSize(30)
+        cell?.textLabel?.textAlignment = .center
+        cell?.textLabel?.text = category[indexPath.row]
+        cell?.textLabel?.textColor = Color(0.6, 0.6, 0.6)
         return cell!
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return category.count
     }
     
     //MARK: - collectionView delegate & datasource
